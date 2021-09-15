3e3194ba8e0076f7012dd0028293ca83aeb88f08
john@starfish.vi <john@starfish.vi>
2021-09-15 15:38:14 -0400

    Add project files.

---
+++ recursive/AddEm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveFib
+{
+    public static class AddEm
+    {
+        public static ulong NewNum { get; set; }
+        public static void NumbersToAdd(ulong num1, ulong num2)
+        { 
+            NewNum = num1 + num2;
+            if (NewNum > ulong.MaxValue / 2)
+            {
+                return;
+            }
+            NumbersToAdd(num2, NewNum);
+            return ;
+        }
+    }
+}
---
+++ recursive/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveFib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = DateTime.Now;
+            AddEm.NumbersToAdd(0, 1);
+            Console.WriteLine($"Value = {AddEm.NewNum} in {(DateTime.Now - start).TotalMilliseconds} milliseconds\nEnter to close");
+            Console.ReadLine();
+        }
+
+    }
+
+
+}
